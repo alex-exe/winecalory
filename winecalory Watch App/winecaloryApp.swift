562b9880987fd98dc7516bf5b8b4f355862dd638
Alex Reshetko <alexr@bemobi.com>
2024-03-09 21:54:19 +0100

    Initial Commit

---
+++ winecalory Watch App/winecaloryApp.swift
@@ -0,0 +1,17 @@
+//
+//  winecaloryApp.swift
+//  winecalory Watch App
+//
+//  Created by Alex Reshetko on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct winecalory_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
